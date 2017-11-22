81862d12df195ce0a511572646300b2e400d9212
developerworks <developerworks@163.com>
2017-11-22 23:45:26 +0800

    first commit

---
+++ src/main/java/hello/MainController.java
@@ -0,0 +1,69 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.io.*;
+import java.util.*;
+
+
+@Controller
+@RequestMapping(path = "/demo")
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path = "/add")
+    @Transactional
+    public @ResponseBody
+    String addNewUser(@RequestParam String name, @RequestParam String email) throws IOException {
+
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+
+        // 创建验证器工程
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        // 实例化验证器
+        Validator validator = factory.getValidator();
+
+        // 验证并获取验证结果
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        // TODO:资源文件在应用程序启动的时候加载并且常驻内存, 适合小文件
+
+        // 检查错误
+        if (violations.size() == 0) {
+            userRepository.save(user);
+            return "Saved.";
+        }
+        // 获取错误提示
+        else {
+            StringBuffer buf = new StringBuffer();
+            InputStream utf8in = getClass().getClassLoader().getResourceAsStream("messages_zh_CN.properties");
+            PropertyResourceBundle bundle = new PropertyResourceBundle(new InputStreamReader(utf8in, "UTF-8"));
+            for (ConstraintViolation<User> violation : violations) {
+                buf.append("-" + bundle.getString(violation.getPropertyPath().toString()));
+                buf.append(violation.getMessage()+"\n");
+            }
+            System.out.println(buf);
+            return "Save error.";
+        }
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/hello/User.java
@@ -0,0 +1,48 @@
+package hello;
+
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Length(min = 6, max = 12)
+    private String name;
+
+    @Email
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
