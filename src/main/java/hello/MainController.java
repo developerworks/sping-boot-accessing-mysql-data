f8f57d86e9f58bcd54c568c71d1ceb2eed8125cb
developerworks <developerworks@163.com>
2017-11-23 00:35:06 +0800

    Fix issues: java 9 with maven

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -3,10 +3,7 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -17,6 +14,7 @@ import java.util.*;
 
 
 @Controller
+//@RestController
 @RequestMapping(path = "/demo")
 public class MainController {
 
