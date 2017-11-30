81afab8bb205b5a1fb4dbee6f45b9514b33a8986
developerworks <developerworks@163.com>
2017-11-30 23:40:48 +0800

    Throw exception when user is null

--- src/main/java/cn/totorotec/service/security/impl/UserDetailsServiceImpl.java
+++ src/main/java/cn/totorotec/service/security/impl/UserDetailsServiceImpl.java
@@ -5,6 +5,8 @@ package cn.totorotec.service.security.impl;
 
 import cn.totorotec.entity.User;
 import cn.totorotec.service.db.UserService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.convert.converter.Converter;
@@ -14,6 +16,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service("userDetailsService")
+@Getter
+@Setter
+
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserService userService;
@@ -21,22 +26,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     @Qualifier(value = "userToUserDetails")
     private Converter<User, UserDetails> userUserDetailsConverter;
-
-    public UserService getUserService() {
-        return userService;
-    }
-
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
-    public Converter<User, UserDetails> getUserUserDetailsConverter() {
-        return userUserDetailsConverter;
-    }
-
-    public void setUserUserDetailsConverter(Converter<User, UserDetails> userUserDetailsConverter) {
-        this.userUserDetailsConverter = userUserDetailsConverter;
-    }
 
     /**
      * 获取用户信息
@@ -47,6 +36,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userUserDetailsConverter.convert(userService.findByUsername(username));
+        User user = userService.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User name " + username + " not found");
+        }
+        return userUserDetailsConverter.convert(user);
     }
 }
